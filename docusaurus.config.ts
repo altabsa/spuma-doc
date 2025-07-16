90b874a6edf6f5b36bdd9f24dc8954d9f2ef5894
WiktoriaJanuszkiewicz <wiktoria.januszkiewicz01@gmail.com>
2025-07-16 15:02:08 +0200

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -102,7 +102,7 @@ const config: Config = {
           type: 'docSidebar',
           sidebarId: 'tutorialSidebar',
           position: 'left',
-          label: 'Tutorial',
+          label: 'Dokumentacja',
         },
         // {to: '/blog', label: 'Blog', position: 'left'},
         // {

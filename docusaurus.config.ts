8ef21914e60ee8ae405535ff4915c91ade986dd7
WiktoriaJanuszkiewicz <wiktoria.januszkiewicz01@gmail.com>
2025-09-24 08:25:34 +0200

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -124,6 +124,11 @@ const config: Config = {
         //   label: 'GitHub',
         //   position: 'right',
         // },
+          {
+            type: 'docsVersionDropdown',
+            position: 'right',
+            dropdownActiveClassDisabled: true,
+         },
       ],
     },
     footer: {

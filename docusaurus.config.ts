ab235ea1cb97e94f7599932986e2e52aba8d4eae
WiktoriaJanuszkiewicz <wiktoria.januszkiewicz01@gmail.com>
2025-09-24 10:20:31 +0200

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -104,7 +104,7 @@ const config: Config = {
   ],
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/spuma-social-card..png',
+    //image: 'img/spuma-social-card..png',
     navbar: {
       title: 'SPUMA',
       logo: {
